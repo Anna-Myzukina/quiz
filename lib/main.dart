87266fa0fe1733edd0a2e72667a51de309cbf168
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-07-27 16:11:58 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Basics flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Testing"),
+      ),
+      body: Container(
+        child: Center(
+          child: Text(
+              'Here will be something soon',
+              
+          ),
+        ),
+      ), 
+    );
+  }
+}
